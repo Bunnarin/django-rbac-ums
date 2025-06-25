a9fb575359995920681bf460708e56ea19db3e05
Bunnarin <chrunbunnarin857@gmail.com>
2025-06-25 13:08:44 +0700

    made the form function cleaner

--- ums/apps/core/forms.py
+++ ums/apps/core/forms.py
@@ -8,73 +8,37 @@ def generate_dynamic_form_class(question_definitions, form_name="DynamicForm"):
     form_fields = {}
 
     for i, field_def in enumerate(question_definitions):
-        field_name = field_def.get('name', f'field_{i}')
         field_type = field_def.get('type')
-        field_title = field_def.get('title')
-        is_required = field_def.get('is_required', False)
+        fieldKwargs = {
+            'label': field_def.get('label'),
+            'required': field_def.get('is_required'),
+        }
+
+        max_length = field_def.get('max_length', None)
+        min_value = field_def.get('min_value', None)
+        max_value = field_def.get('max_value', None)
+        choices = [(item, item) for item in field_def.get('choices', [])]
+
+        if max_length:
+            fieldKwargs['max_length'] = max_length
+        if min_value:
+            fieldKwargs['min_value'] = min_value
+            fieldKwargs['max_value'] = max_value
+        if choices:
+            fieldKwargs['choices'] = choices
 
         form_field = None
-
-        if field_type == 'text':
-            form_field = forms.CharField(
-                label=field_title,
-                required=is_required,
-                max_length=field_def.get('maxLength'),
-            )
-        elif field_type == 'paragraph':
-            form_field = forms.CharField(
-                label=field_title,
-                required=is_required,
-                max_length=field_def.get('maxLength'),
-                widget=forms.Textarea,
-            )
-        elif field_type == 'integer':
-            min_val = field_def.get('min')
-            max_val = field_def.get('max')
-
-            form_field = forms.IntegerField(
-                label=field_title,
-                required=is_required,
-                min_value=min_val,
-                max_value=max_val,
-            )
-        elif field_type == 'decimal':
-            min_val = field_def.get('min')
-            max_val = field_def.get('max')
-
-            form_field = forms.FloatField(
-                label=field_title,
-                required=is_required,
-                min_value=min_val,
-                max_value=max_val,
-            )
-
-        elif field_type == 'dropdown':
-            choices_list = [(item, item) for item in field_def.get('choices', [])]
-            form_field = forms.ChoiceField(
-                label=field_title,
-                required=is_required,
-                choices=choices_list,
-            )
-
-        elif field_type == 'checkbox': # Multiple choice using SelectMultiple (multi-select dropdown)
-            choices_list = [(item, item) for item in field_def.get('choices', [])]
-            form_field = forms.MultipleChoiceField(
-                label=field_title,
-                required=is_required,
-                choices=choices_list,
-                widget=forms.CheckboxSelectMultiple(),
-            )
-
-        elif field_type == 'date':
-            form_field = forms.DateField(
-                label=field_title,
-                required=is_required,
-                widget=forms.DateInput(attrs={'type': 'date'}), # Renders as an HTML5 date picker
-            )
+        match field_type:
+            case 'text': form_field = forms.CharField(**fieldKwargs)
+            case 'paragraph': form_field = forms.CharField(widget=forms.Textarea(), **fieldKwargs)
+            case 'integer': form_field = forms.IntegerField(**fieldKwargs)
+            case 'decimal': form_field = forms.FloatField(**fieldKwargs)
+            case 'dropdown': form_field = forms.ChoiceField(**fieldKwargs)
+            case 'checkbox': form_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), **fieldKwargs)
+            case 'date': form_field = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), **fieldKwargs)
 
         if form_field:
-            form_fields[field_name] = form_field
+            form_fields[i] = form_field
 
     DynamicForm = type(
         form_name,
