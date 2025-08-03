ce2c0e3e19694aae6febc526289e448e6ea72a2d
Bunnarin <chrunbunnarin857@gmail.com>
2025-08-03 09:08:27 +0700

    patched entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+python manage.py createcachetable
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py create_super_user
 python manage.py crontab add
-python manage.py createcachetable
 gunicorn --chdir ums ums.wsgi:application --bind 0.0.0.0:8000
