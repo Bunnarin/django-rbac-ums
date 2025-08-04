0972dd036ec6d36a269a90bc1fd1667037e14f8e
Bunnarin <chrunbunnarin857@gmail.com>
2025-08-04 14:33:56 +0700

    now zero perm group are no longer invisible in the queryset

--- ums/apps/users/queryset.py
+++ ums/apps/users/queryset.py
@@ -7,12 +7,9 @@ class GroupQuerySet(models.QuerySet):
         """
         Returns groups where all permissions are a subset of the user's group permissions.
         """
-        if user.is_superuser:
-            return self
-            
         user_group_perm_ids = set(Permission.objects.filter(group__user=user).values_list('id', flat=True))
         
         return self.annotate(
             total_permissions=Count('permissions'),
             user_has_permissions=Count('permissions', filter=Q(permissions__id__in=user_group_perm_ids))
-        ).filter(total_permissions=F('user_has_permissions'))
+        ).filter(Q(total_permissions=0) | Q(total_permissions=F('user_has_permissions')))
